b42b40df62a1c6a7f209945b328839afd3e6ea51
yusuf <yk575155@gmail.com>
2026-03-20 16:44:46 +0530

    complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "./style.css"
+import "./style.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
